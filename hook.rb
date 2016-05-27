708a6a9392197ca674c60f76fb6beddceb414125
Lucas Perez <lucasmpb@gmail.com>
2016-05-27 11:47:37 -0300

    Log incoming request to /hook endpoint

---
+++ hook.rb
@@ -0,0 +1,18 @@
+#sudo ruby hook.rb
+
+require 'sinatra'
+require 'json'
+require 'logger'
+
+set :port, 80
+
+get '/' do
+  '...'
+end
+
+post '/hook' do
+  logger = Logger.new('logfile.log')
+  logger.level = Logger::DEBUG
+  body = JSON.parse(request.body.read)
+  logger.debug(event: body, sift_signature: request.env["HTTP_X_SIFT_SCIENCE_SIGNATURE"])
+end
